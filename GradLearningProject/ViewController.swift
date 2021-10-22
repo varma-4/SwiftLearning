4993e3555785f50fe61305304c23e218939eeeb1
Mani <manikanta.datla@team.telstra.com>
2021-10-22 14:43:58 +0530

    Fixed some SwiftLint warnings for RoundedButton class Renamed enum name in VIewController file, Also Modularised methods to allow TDD Enabled Test coverage for the project Added XCTest for calculator maximum numbers shown on the display

--- GradLearningProject/ViewController.swift
+++ GradLearningProject/ViewController.swift
@@ -7,12 +7,19 @@
 
 import UIKit
 
-enum Operation: String {
+enum OperatorKeys: String {
     case add = "+"
     case substract = "-"
     case divide = "/"
     case multiply = "x"
     case NULL = "Null"
+}
+
+enum Numbers: Int {
+    case zero
+    case one
+    case two
+    case three
 }
 
 class ViewController: UIViewController {
@@ -24,7 +31,7 @@ class ViewController: UIViewController {
     var leftValue = ""
     var rightValue = ""
     var result = ""
-    var operation: Operation = .NULL
+    var operation: OperatorKeys = .NULL
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -32,10 +39,22 @@ class ViewController: UIViewController {
     }
     // 0-9 Button Actions
     @IBAction func numbers(_ sender: RoundButton) {
+        // logic ... label
+        numberTapped(button: sender)
+        // connecting to the label
+        if label.text != numberOnScreen {
+            label.text = numberOnScreen
+        }
+    }
+
+    /// Appending tapped number to the calculator display
+    /// - Parameter button: should be an instance of rounded button, with a tag set
+    func numberTapped(button: RoundButton) {
+        // description
+        // Business scenario 1, at max you need to have 9 numbers displayed
         if numberOnScreen.count <= 8 {
-        numberOnScreen += "\(sender.tag)"
-        label.text = numberOnScreen
-   }
+            numberOnScreen += "\(button.tag)"
+        }
     }
     // All Clear Button Action
     @IBAction func allClear(_ sender: RoundButton) {
@@ -49,7 +68,6 @@ class ViewController: UIViewController {
     // Divide Operation
     @IBAction func divOp(_ sender: RoundButton) {
         operationfunc(oper: .divide)
-
     }
     // Multiplication
     @IBAction func mulOp(_ sender: RoundButton) {
@@ -68,52 +86,23 @@ class ViewController: UIViewController {
     // Equal To Button
     @IBAction func equal(_ sender: RoundButton) {
         operationfunc(oper: operation)
-
     }
     // Dot button
     @IBAction func dot(_ sender: RoundButton) {
         if numberOnScreen.count <= 7 {
-        numberOnScreen += "."
-        label.text = numberOnScreen
-    }
+            numberOnScreen += "."
+            label.text = numberOnScreen
+        }
     }
     // Performing Calculation
-    func operationfunc(oper: Operation) {
+    func operationfunc(oper: OperatorKeys) {
         var left = 0.0
         var right = 0.0
         var res = 0.0
+        // First check if the operation is not null
+        // bussiness rule
         if operation != .NULL {
-            if numberOnScreen != ""{
-                // If operation is not null and there is some number on screen
-                rightValue = numberOnScreen
-                numberOnScreen = ""
-                if let leftv = Double(leftValue) {
-                    left = leftv
-                }
-                if let rightv = Double(rightValue) {
-                    right = rightv
-                }
-                switch operation {
-                case .add : result = "\(left + right)"
-                case .substract : // Performing Sustraction
-                                    result = "\(left - right)"
-                case .multiply : // Performing Multiplication
-                                    result = "\(left * right)"
-                case .divide : // Performing Division
-                                    result = "\(left / right)"
-                case .NULL : print("No operation")
-                }
-                if let resultv = Double(result) {
-                    res = resultv
-                }
-                leftValue = result
-                if res.truncatingRemainder(dividingBy: 1) == 0 {
-                    // if the numbers are integer then converting the result back to integer
-                    result = "\(Int(res))"
-                }
-                // printing result
-                label.text = result
-            }
+            method1()
             // storing the operation for further calculation
             operation = oper
         } else {
@@ -121,6 +110,43 @@ class ViewController: UIViewController {
             leftValue = numberOnScreen
             numberOnScreen = ""
             operation = oper
+        }
+    }
+
+    func method1() {
+        var left = 0.0
+        var right = 0.0
+        var res = 0.0
+        if numberOnScreen != ""{
+            // If operation is not null and there is some number on screen
+            rightValue = numberOnScreen
+            numberOnScreen = ""
+            if let leftv = Double(leftValue) {
+                left = leftv
+            }
+            if let rightv = Double(rightValue) {
+                right = rightv
+            }
+            switch operation {
+            case .add : result = "\(left + right)"
+            case .substract : // Performing Sustraction
+                                result = "\(left - right)"
+            case .multiply : // Performing Multiplication
+                                result = "\(left * right)"
+            case .divide : // Performing Division
+                                result = "\(left / right)"
+            case .NULL : print("No operation")
+            }
+            if let resultv = Double(result) {
+                res = resultv
+            }
+            leftValue = result
+            if res.truncatingRemainder(dividingBy: 1) == 0 {
+                // if the numbers are integer then converting the result back to integer
+                result = "\(Int(res))"
+            }
+            // printing result
+            label.text = result
         }
     }
 }
--- GradLearningProjectTests/GradLearningProjectTests.swift
+++ GradLearningProjectTests/GradLearningProjectTests.swift
@@ -30,4 +30,26 @@ class GradLearningProjectTests: XCTestCase {
         }
     }
 
+    func testCalculatorMinusOperatorLogic() {
+        // TDD Test driven development
+        // if your test never fails, then you are writing the wrong test.
+        let viewController = ViewController()
+        var buttons = [RoundButton]()
+        for number in 0..<10 {
+            let object = RoundButton()
+            object.tag = number
+            buttons.append(object)
+        }
+        XCTAssertTrue(buttons.count == 10)
+        XCTAssertEqual(buttons.count, 10, "Button count should be ten")
+        for number in 0...11 {
+            let button = number > (buttons.count - 1) ? buttons[0] : buttons[number]
+            viewController.numberTapped(button: button)
+        }
+        XCTAssertEqual(viewController.numberOnScreen.count, 9)
+    }
+
+    // logic
+    // UI
+
 }
