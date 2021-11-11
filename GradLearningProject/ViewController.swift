39fa604053bc6a26b330f0c978d83271d950a0dd
samrudhi27 <samrudhisantaji.cs18@rvce.edu.in>
2021-11-11 17:52:57 +0530

    Installed Carthage integrated Alamofire into the project

--- GradLearningProject/ViewController.swift
+++ GradLearningProject/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getposts()
+        print("Hi")
+        
     }
+    func getposts() {
+        Alamofire.request("https://jsonplaceholder.typicode.com/posts").responseJSON {
+            response in
+            //Original URL request
+            print(response.request)
+            //HTTP URL response
+            print(response.response)
+            
+            print(response.data)
+            print(response.result)
+            if let JSON = response.result.value{
+                print("JSON: \(JSON)")
+            }
+        }
 
+    }
 
 }
 
